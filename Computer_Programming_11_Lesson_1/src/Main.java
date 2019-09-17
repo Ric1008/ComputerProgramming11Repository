3b72c661e94df5646e1e01b80c53b92ac746fecd
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-09-17 10:39:23 -0700

    Push

---
+++ Computer_Programming_11_Lesson_1/src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println((-4)/3);
+
+                //Expression 1
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println(5+10/2-(4-8)/3);
+        System.out.println(5+10/2-(-4)/3);
+        System.out.println(5+5-(-4)/3);
+        System.out.println(5+5-(-1));
+        System.out.println(10-(-1));
+        System.out.println(11);
+
+        System.out.println(3/2);
+
+        //Expression 2
+        System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(23%4 + (18 - 1));
+        System.out.println(23%4 + 17);
+        System.out.println(3 + 17);
+        System.out.println(20);
+
+        System.out.println(81.0/5);
+
+        //Expression 3
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println((3 + Math.pow(3,4)/5));
+        System.out.println((3 + 81.0/5));
+        System.out.println((3 + 16.2));
+        System.out.println((19.2));
+
+        System.out.println(1);
+
+        //Expression 4
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (9.0 / 3));
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + 3.0);
+        System.out.println(1 - 8 + 8.0 / 8 + 3.0);
+        System.out.println(1 - 8 + 1.0 + 3.0);
+        System.out.println(-7 + 1.0 + 3.0);
+        System.out.println(-6.0 + 3.0);
+        System.out.println(-3.0);
+
+        System.out.println(1);
+
+        //Expression 5
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+
+    }
+}
