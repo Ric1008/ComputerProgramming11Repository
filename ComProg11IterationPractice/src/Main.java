0c0079a4fede2e378097ccc14ca32c4e8ab9690b
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-10-11 11:35:24 -0700

    Iteration Practice
    Practice for iteration and iteration testing.

---
+++ ComProg11IterationPractice/src/Main.java
@@ -0,0 +1,19 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+import com.sun.org.apache.xpath.internal.objects.XStringForChars;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int s=0;
+
+Scanner scan=new Scanner(System.in);
+
+
+do{
+s=Integer.parseInt(scan.next());
+}
+while(s!=7);
+
+    }
+}
