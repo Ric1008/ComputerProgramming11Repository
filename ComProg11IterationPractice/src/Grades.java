12a92eb342d0eff710872bb0adb2886c7268c49f
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-10-18 11:38:18 -0700

    More Practice
    Getting there.

--- ComProg11IterationPractice/src/Grades.java
+++ ComProg11IterationPractice/src/Grades.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Grades {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-int number=0;
-int grade=0
+        int number=0;
+int grade=0;
+
+
+
+        System.out.println("type the number of grades you wish to calculate with");
+        number=Integer.parseInt(scan.next());
+for(int i=0; i< number ;i++){
+    System.out.println("put in a number grade");
+    grade=Integer.parseInt(scan.next());
+}
+        System.out.println(grade);
+
     }
 }
