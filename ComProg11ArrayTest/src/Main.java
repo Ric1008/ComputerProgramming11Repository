fdeec29dff8e04229ba8e0ae6b08789df81ff463
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-11-11 13:58:30 -0800

    yep
    not sure what this is

--- ComProg11ArrayTest/src/Main.java
+++ ComProg11ArrayTest/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] integers=new int[5];
         System.out.println(integers);
+        integers[2]=10;
+        System.out.println(Arrays.toString(integers));
+        int[] arr=new int[5];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i*2;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int[] twenty=new int[20];
+        for(int a=0; a<twenty.length; a++){
+            
+        }
     }
 }
---
+++ ComProg11ArrayTest/src/Testtwo.java
@@ -0,0 +1,6 @@
+public class Testtwo {
+    public static void main(String[] args) {
+        int[] integers=new int[5];
+
+    }
+}
