7692f1c377988ef6c0f8253bd86e6ff2b15b8aa7
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-11-26 09:59:39 -0800

    Project 1
    Working

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     static int problemOne(String s){
         int answer = 0;
         //your code here
+        for(int i=0; i<s.length(); i++){
+            if(s.substring()=="a"||s.substring()=="e"||s.substring()=="i"||s.substring()=="o"||s.substring()=="u"){
+answer+=1;
+            }
+        }
         return answer;
     }
     //Code you problem number two here
@@ -11,7 +16,7 @@ public class Main {
         //your code here
         return answer;
     }
-    //Code your solution to problem number 3 here
+    //Code your solution to problem number 3 he0re
     static String problemThree(String s){
         //your code here
         return s;
@@ -25,5 +30,18 @@ public class Main {
         all possible cases.  If you have 100% success then there is no bugs in your methods.
          */
         String s;
+        System.out.println('c'<'d');
+        System.out.println('j'+'j');
+        System.out.println('a');
+        System.out.println('b');
+        System.out.println('c');
+        System.out.println('d');
+        System.out.println('e');
+        System.out.println('f');
+        System.out.println('g');
+        System.out.println('j');
+        System.out.println('J');
+        String run="hum";
+
     }
 }
