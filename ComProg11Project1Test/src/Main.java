3d8cef3eafebcb4b67d1758d7878fe9a49cb1470
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-12-02 01:26:12 -0800

    Problem 3
    Damn

---
+++ ComProg11Project1Test/src/Main.java
@@ -0,0 +1,48 @@
+
+// Java to find the number of substrings
+// in alphabetical order
+import java.util.*;
+public class Main
+{
+
+    // Function to find number of substrings
+    static int findSubstringCount(String str)
+    {
+        int result = 0;
+        int n = str.length();
+        int counter=0;
+
+        // Iterate over string length
+        for (int i = 0; i < n - 1; i++) {
+            // if any two chars are in alphabetic order
+            if (str.charAt(i) + 1 == str.charAt(i+1)) {
+                result++;
+                counter++;
+                // find next char not in order
+                while (str.charAt(i) + 1 == str.charAt(i+1)) {
+                    i++;
+                    counter++;
+                }
+            }
+        }
+
+        // return the result
+        return result;
+    }
+
+    // Driver function
+    public static void main(String args[])
+    {
+        String str = "abcafadfadsabcdefghiiiuijyggg";
+
+        System.out.println(findSubstringCount(str));
+        System.out.println(str.charAt(2));
+        String a="bone";
+        a="pizza";
+        System.out.println(a);
+
+    }
+
+}
+//contributed by Arnab Kundu
+
--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -4,24 +4,55 @@ public class Main {
         int answer = 0;
         int vowel=0;
         //your code here
-        for(int i=0; i<s.length(); i++){
-            if(s.substring(i)=="a"||s.substring(i)=="e"||s.substring(i)=="i"||s.substring(i)=="o"||s.substring(i)=="u"){
-vowel+=1;
-return answer;
+for(int i=0; i<s.length(); i++)
+            if(s.substring(i,i+1).equals("a")||s.substring(i,i+1).equals("e")||s.substring(i,i+1).equals("i")||s.substring(i,i+1).equals("o")||s.substring(i,i+1).equals("u")){
+                answer+=1;
             }
-            return answer;
-        }
         return answer;
-    }
+        }
+
+
     //Code you problem number two here
     static int problemTwo(String s){
         int answer = 0;
         //your code here
+        for(int i=0; i<s.length()-2; i++)
+            if(s.substring(i,i+3).equals("bob")){
+                answer+=1;
+            }
         return answer;
     }
     //Code your solution to problem number 3 he0re
-    static String problemThree(String s){
+    static String problemThree(String s) {
         //your code here
+        int result = 0;
+        int n = s.length();
+        int counter = 0;
+        int big=0;
+
+
+        // Iterate over string length
+        for (int i = 0; i < s.length() - 1; i++) {
+            // if any two chars are in alphabetic order
+            if (s.charAt(i) + 1 == s.charAt(i + 1)) {
+
+                counter++;
+                // find next char not in order
+                while (s.charAt(i) + 1 == s.charAt(i + 1)) {
+                    i++;
+                    counter++;
+                }
+
+            }
+            if(counter>big){
+                counter=big;
+            }
+            for (int a = 0; a < s.substring(a,a+big).length() - 1; a++){
+                char[] chain=a;
+            char[a] chain = s.charAt(a);
+            }
+        }
+
         return s;
     }
     public static void main(String[] args) {
@@ -32,20 +63,28 @@ return answer;
         The tests will put your method through several different Strings to test
         all possible cases.  If you have 100% success then there is no bugs in your methods.
          */
+        String s="uiegahhdjafeawhfehafhehfiaehfehafhhabcdefggghehaufheahghahdv";
+
         System.out.println();
-        String s;
         System.out.println('c'<'d');
-        System.out.println('j'+'j');
-        System.out.println('a');
-        System.out.println('b');
-        System.out.println('c');
+        System.out.println('c'+'d');
         System.out.println('d');
-        System.out.println('e');
-        System.out.println('f');
-        System.out.println('g');
-        System.out.println('j');
-        System.out.println('J');
-        String run="hum";
+        System.out.println('a'+'b');
+        System.out.println('a'+'a');
+        System.out.println('a'+'q');
+        System.out.println('a'+'a'+'a');
+        System.out.println('a');
+        System.out.println('a'+'a');
+        System.out.println('a'+'a'+'a');
+        System.out.println('a'+'a'+'a'+'a');
+        System.out.println('a'+'a'+'a'+'a'+'a');
+        System.out.println('a'<'b');
+        System.out.println(problemThree(s));
+
+
+
+
+
 
     }
 }
