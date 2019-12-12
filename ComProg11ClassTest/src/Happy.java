b3d858af559639fe23bde2f7cc571b99877a93ec
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2019-12-12 10:01:02 -0800

    Classes
    Start Classes and such.

---
+++ ComProg11ClassTest/src/Happy.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class Happy {
+    public static void main(String[] args) {
+        System.out.println(ham);
+    }
+}
---
+++ ComProg11ClassTest/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }public int ham=1;
+}
+class pox {}
